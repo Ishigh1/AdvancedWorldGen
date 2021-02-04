7582628fde0223e57468eaf7136f0c6f587f17fa
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2021-02-04 17:33:11 +0100

    Update AdvancedSeedGen.cs

--- AdvancedSeedGen.cs
+++ AdvancedSeedGen.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using AdvancedSeedGen.SeedUI;
+using AdvancedSeedGen.SpecialSeeds;
 using Newtonsoft.Json;
 using On.Terraria;
 using On.Terraria.GameContent.UI.States;
@@ -15,26 +16,27 @@ namespace AdvancedSeedGen
 	public class AdvancedSeedGen : Mod
 	{
 		public static List<string> Options;
-		public static Dictionary<string, List<string>> SeedTranslator;
 		public static List<int> NotReplaced;
-		public SeedHelper SeedHelper;
 		public CancelWorldGen CancelWorldGen;
+		public SeedHelper SeedHelper;
 
 		public override void Load()
 		{
 			NotReplaced = new List<int>
 			{
-				ClosedDoor, MagicalIceBlock, Traps, Boulder, Teleporter, PlanterBox, TallGateClosed
+				ClosedDoor, MagicalIceBlock, Traps, Boulder, Teleporter, MetalBars, PlanterBox, TrapdoorClosed, TallGateClosed
 			};
-			SeedTranslator = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(
+			SeedHelper.SeedTranslator = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(
 				Encoding.UTF8.GetString(GetFileBytes("SeedTranslation.json")));
 			TileReplacer.Initialize();
 
 			CancelWorldGen = new CancelWorldGen();
-			
+
 			Main.OnSeedSelected += CancelWorldGen.WorldFileDataOnSetSeed;
 			UIWorldLoad.ctor += CancelWorldGen.AddCancel;
 			UIElement.Append += CancelWorldGen.StealProgressBar;
+			UIVirtualKeyboard.ctor += SeedSelector.AddSeedInterface;
+
 			Main.checkXMas += SnowWorld.MainOncheckXMas;
 			Projectile.Kill += SnowWorld.RemoveSnowDropDuringChristmas;
 		}
@@ -44,14 +46,15 @@ namespace AdvancedSeedGen
 			Main.OnSeedSelected -= CancelWorldGen.WorldFileDataOnSetSeed;
 			UIWorldLoad.ctor -= CancelWorldGen.AddCancel;
 			UIElement.Append -= CancelWorldGen.StealProgressBar;
+			UIVirtualKeyboard.ctor += SeedSelector.AddSeedInterface;
+
 			Main.checkXMas -= SnowWorld.MainOncheckXMas;
 			Projectile.Kill -= SnowWorld.RemoveSnowDropDuringChristmas;
-			
-			CancelWorldGen = null;
-			
+
 			TileReplacer.Unload();
 			NotReplaced = null;
-			SeedTranslator = null;
+			Options = null;
+			SeedHelper.SeedTranslator = null;
 		}
 
 		public override void HandlePacket(BinaryReader reader, int whoAmI)
