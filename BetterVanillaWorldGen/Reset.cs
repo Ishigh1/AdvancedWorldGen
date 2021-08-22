e6b433af924e508568e72bb4d0f60e273478b353
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2021-08-22 20:50:22 +0200

    Update Reset.cs

--- BetterVanillaWorldGen/Reset.cs
+++ BetterVanillaWorldGen/Reset.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.IO;
 using Terraria.WorldBuilding;
-using static Terraria.WorldGen;
 
 namespace AdvancedWorldGen.BetterVanillaWorldGen
 {
@@ -17,13 +16,13 @@ namespace AdvancedWorldGen.BetterVanillaWorldGen
 		protected override void ApplyPass(GenerationProgress progress, GameConfiguration passConfig)
 		{
 			Replacer.VanillaInterface.NumOceanCaveTreasure.Set(0);
-			skipDesertTileCheck = false;
-			gen = true;
+			WorldGen.skipDesertTileCheck = false;
+			WorldGen.gen = true;
 			Liquid.ReInit();
-			noTileActions = true;
+			WorldGen.noTileActions = true;
 			progress.Message = "";
-			SetupStatueList();
-			RandomizeWeather();
+			WorldGen.SetupStatueList();
+			WorldGen.RandomizeWeather();
 			Main.cloudAlpha = 0f;
 			Main.maxRaining = 0f;
 			Main.raining = false;
@@ -32,85 +31,85 @@ namespace AdvancedWorldGen.BetterVanillaWorldGen
 			Main.checkHalloween();
 			typeof(WorldGen).GetMethod("ResetGenerator", BindingFlags.Static | BindingFlags.NonPublic)
 				.Invoke(null, null);
-			UndergroundDesertLocation = Rectangle.Empty;
-			UndergroundDesertHiveLocation = Rectangle.Empty;
-			numLarva = 0;
+			WorldGen.UndergroundDesertLocation = Rectangle.Empty;
+			WorldGen.UndergroundDesertHiveLocation = Rectangle.Empty;
+			WorldGen.numLarva = 0;
 			Chest.ShuffleChests();
 
 			const int num917 = 86400;
-			Main.slimeRainTime = -genRand.Next(num917 * 2, num917 * 3);
-			Main.cloudBGActive = -genRand.Next(8640, num917);
+			Main.slimeRainTime = -WorldGen.genRand.Next(num917 * 2, num917 * 3);
+			Main.cloudBGActive = -WorldGen.genRand.Next(8640, num917);
 			Replacer.VanillaInterface.SkipFramingDuringGen.Set(false);
-			if (genRand.Next(2) == 0)
+			if (WorldGen.genRand.Next(2) == 0)
 			{
 				Replacer.VanillaInterface.Copper.Set(166);
-				copperBar = 703;
-				SavedOreTiers.Copper = 166;
+				WorldGen.copperBar = 703;
+				WorldGen.SavedOreTiers.Copper = 166;
 			}
 			else
 			{
-				SavedOreTiers.Copper = 7;
-				copperBar = 20;
+				WorldGen.SavedOreTiers.Copper = 7;
+				WorldGen.copperBar = 20;
 			}
 
-			if (genRand.Next(2) == 0)
+			if (WorldGen.genRand.Next(2) == 0)
 			{
 				Replacer.VanillaInterface.Iron.Set(167);
-				ironBar = 704;
-				SavedOreTiers.Iron = 167;
+				WorldGen.ironBar = 704;
+				WorldGen.SavedOreTiers.Iron = 167;
 			}
 			else
 			{
-				SavedOreTiers.Iron = 6;
-				ironBar = 22;
+				WorldGen.SavedOreTiers.Iron = 6;
+				WorldGen.ironBar = 22;
 			}
 
-			if (genRand.Next(2) == 0)
+			if (WorldGen.genRand.Next(2) == 0)
 			{
 				Replacer.VanillaInterface.Silver.Set(168);
-				silverBar = 705;
-				SavedOreTiers.Silver = 168;
+				WorldGen.silverBar = 705;
+				WorldGen.SavedOreTiers.Silver = 168;
 			}
 			else
 			{
-				SavedOreTiers.Silver = 9;
-				silverBar = 21;
+				WorldGen.SavedOreTiers.Silver = 9;
+				WorldGen.silverBar = 21;
 			}
 
-			if (genRand.Next(2) == 0)
+			if (WorldGen.genRand.Next(2) == 0)
 			{
 				Replacer.VanillaInterface.Gold.Set(169);
-				goldBar = 706;
-				SavedOreTiers.Gold = 169;
+				WorldGen.goldBar = 706;
+				WorldGen.SavedOreTiers.Gold = 169;
 			}
 			else
 			{
-				SavedOreTiers.Gold = 8;
-				goldBar = 19;
+				WorldGen.SavedOreTiers.Gold = 8;
+				WorldGen.goldBar = 19;
 			}
 
-			crimson = WorldGenParam_Evil switch
+			WorldGen.crimson = WorldGen.WorldGenParam_Evil switch
 			{
 				0 => false,
 				1 => true,
-				_ => genRand.Next(2) == 0
+				_ => WorldGen.genRand.Next(2) == 0
 			};
 
-			Main.worldID = genRand.Next(int.MaxValue);
-			RandomizeTreeStyle();
-			RandomizeCaveBackgrounds();
-			RandomizeBackgrounds(genRand);
-			RandomizeMoonState();
-			TreeTops.CopyExistingWorldInfoForWorldGeneration();
+			Main.worldID = WorldGen.genRand.Next(int.MaxValue);
+			WorldGen.RandomizeTreeStyle();
+			WorldGen.RandomizeCaveBackgrounds();
+			WorldGen.RandomizeBackgrounds(WorldGen.genRand);
+			WorldGen.RandomizeMoonState();
+			WorldGen.TreeTops.CopyExistingWorldInfoForWorldGeneration();
 
-			int dungeonSide = genRand.Next(2) == 0 ? 1 : -1;
+			int dungeonSide = WorldGen.genRand.Next(2) == 0 ? 1 : -1;
 			Replacer.VanillaInterface.DungeonSide.Set(dungeonSide);
 
-			int shift = (int) (Main.maxTilesX * genRand.Next(15, 30) * 0.01f);
+			int shift = (int) (Main.maxTilesX * WorldGen.genRand.Next(15, 30) * 0.01f);
 			Replacer.VanillaInterface.JungleOriginX.Set(dungeonSide == 1 ? shift : Main.maxTilesX - shift);
 
-			int snowCenter = genRand.Next((int) (Main.maxTilesX * 0.85f));
-			if (dungeonSide == 1 && !drunkWorldGen)
+			int snowCenter = WorldGen.genRand.Next((int) (Main.maxTilesX * 0.85f));
+			if (dungeonSide == 1 && !WorldGen.drunkWorldGen)
 			{
 				if (snowCenter > Main.maxTilesX * 0.6f)
 					snowCenter += (int) (Main.maxTilesX * 0.15f);
@@ -121,15 +120,15 @@ namespace AdvancedWorldGen.BetterVanillaWorldGen
 					snowCenter += (int) (Main.maxTilesX * 0.15f);
 			}
 
-			int num921 = genRand.Next(50, 90);
+			int num921 = WorldGen.genRand.Next(50, 90);
 			float num922 = Main.maxTilesX / 4200f;
-			num921 += (int) (genRand.Next(20, 40) * num922);
-			num921 += (int) (genRand.Next(20, 40) * num922);
+			num921 += (int) (WorldGen.genRand.Next(20, 40) * num922);
+			num921 += (int) (WorldGen.genRand.Next(20, 40) * num922);
 			int snowOriginLeft = Math.Max(0, snowCenter - num921);
 
-			num921 = genRand.Next(50, 90);
-			num921 += (int) (genRand.Next(20, 40) * num922);
-			num921 += (int) (genRand.Next(20, 40) * num922);
+			num921 = WorldGen.genRand.Next(50, 90);
+			num921 += (int) (WorldGen.genRand.Next(20, 40) * num922);
+			num921 += (int) (WorldGen.genRand.Next(20, 40) * num922);
 			int snowOriginRight = Math.Min(Main.maxTilesX, snowCenter + num921);
 
 			Replacer.VanillaInterface.SnowOriginLeft.Set(snowOriginLeft);
@@ -141,23 +140,21 @@ namespace AdvancedWorldGen.BetterVanillaWorldGen
 			int beachBordersWidth = (int) (275 * worldSize);
 			int beachSandRandomWidthRange = (int) (20 * worldSize);
 			int beachSandRandomCenter = beachBordersWidth + 5 + 2 * beachSandRandomWidthRange;
-			oceanDistance = beachBordersWidth - 25;
-			beachDistance = beachSandRandomCenter + beachSandDungeonExtraWidth + beachSandJungleExtraWidth;
+			WorldGen.oceanDistance = beachBordersWidth - 25;
+			WorldGen.beachDistance = beachSandRandomCenter + beachSandDungeonExtraWidth + beachSandJungleExtraWidth;
 			
-			int leftBeachEnd = beachSandRandomCenter +
-			                   genRand.Next(-beachSandRandomWidthRange, beachSandRandomWidthRange);
+			int leftBeachEnd = beachSandRandomCenter + WorldGen.genRand.Next(-beachSandRandomWidthRange, beachSandRandomWidthRange);
 			leftBeachEnd += dungeonSide == 1 ? beachSandDungeonExtraWidth : beachSandJungleExtraWidth;
 			Replacer.VanillaInterface.LeftBeachEnd.Set(leftBeachEnd);
 
-			int rightBeachStart = Main.maxTilesX - beachSandRandomCenter +
-			                      genRand.Next(-beachSandRandomWidthRange, beachSandRandomWidthRange);
+			int rightBeachStart = Main.maxTilesX - beachSandRandomCenter + WorldGen.genRand.Next(-beachSandRandomWidthRange, beachSandRandomWidthRange);
 			rightBeachStart -= dungeonSide == -1 ? beachSandDungeonExtraWidth : beachSandJungleExtraWidth;
 			Replacer.VanillaInterface.RightBeachStart.Set(rightBeachStart);
 
 			const int num925 = 50;
 			Replacer.VanillaInterface.DungeonLocation.Set(dungeonSide == -1
-				? genRand.Next(leftBeachEnd + num925, (int) (Main.maxTilesX * 0.2))
-				: genRand.Next((int) (Main.maxTilesX * 0.8), rightBeachStart - num925));
+				? WorldGen.genRand.Next(leftBeachEnd + num925, (int) (Main.maxTilesX * 0.2))
+				: WorldGen.genRand.Next((int) (Main.maxTilesX * 0.8), rightBeachStart - num925));
 		}
 	}
 }
