65a1574609dda9bf197b973a04aa8c6fbc2b806e
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2021-12-29 20:23:32 +0100

    Seed->Option

--- Base/API.cs
+++ Base/API.cs
@@ -6,21 +6,22 @@ namespace AdvancedWorldGen.Base;
 public class API
 {
 	/// <summary>
-	///     Registers a new seed
+	///     Registers a new option
 	///     To be well drawn, an entry Mods.{mod.Name}.{internalName} defining the displayed option name and an entry
 	///     Mods.{mod.Name}.{internalName}.Description giving a short description must be defined on a localization file
 	/// </summary>
 	/// <param name="mod">The mod featuring the given option</param>
 	/// <param name="internalName">The internal name of the option, it must be unique and with no space</param>
 	/// <param name="hidden">Set to true if you don't want this option to be shown by default</param>
-	public static void RegisterSeed(Mod mod, string internalName, bool hidden = false)
+	public static void RegisterOption(Mod mod, string internalName, bool hidden = false)
 	{
 		Option.OptionDict.Add(internalName, new Option
 		{
 			Children = new List<Option>(),
 			Conflicts = new List<string>(),
 			Hidden = hidden,
-			ModName = mod.Name
+			ModName = mod.Name,
+			Name = internalName
 		});
 	}
 
