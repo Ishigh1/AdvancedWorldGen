40ea0f6551e311cf7f504e7e1379b09a30383ace
Ishigh <45936615+Ishigh1@users.noreply.github.com>
2024-03-18 01:18:27 +0100

    Adding a failsafe for weird conditions (probably onedrive related ?)

--- Base/Legacy.cs
+++ Base/Legacy.cs
@@ -8,15 +8,21 @@ public class Legacy
 
 		string advancedWorldGenPassesDataPath = Path.Combine(Main.SavePath, "AdvancedWorldGenPassesData.json");
 		if (File.Exists(advancedWorldGenPassesDataPath))
-			File.Move(advancedWorldGenPassesDataPath, ModifiedWorld.DataPath);
+			if (File.Exists(ModifiedWorld.DataPath))
+				File.Delete(advancedWorldGenPassesDataPath);
+			else
+				File.Move(advancedWorldGenPassesDataPath, ModifiedWorld.DataPath);
 
 		#endregion
 
 		#region 3.1, 04/04/2023
 
 		string presetDataPath = Path.Combine(AdvancedWorldGenMod.FolderPath, "Presets.json");
-		if (File.Exists(presetDataPath)) 
-			File.Move(presetDataPath, PresetUI.DataPath);
+		if (File.Exists(presetDataPath))
+			if (File.Exists(PresetUI.DataPath))
+				File.Delete(presetDataPath);
+			else
+				File.Move(presetDataPath, PresetUI.DataPath);
 
 		#endregion
 	}
